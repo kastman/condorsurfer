d9193fd06dc1e98e1e855dbbc7d3b290cd61d96b
Erik <ekk@medicine.wisc.edu>
2011-08-11 17:42:53 -0500

    Update Segments and Datasets Views

--- sinatra/webapp.rb
+++ sinatra/webapp.rb
@@ -6,6 +6,8 @@ helpers do
   def tab_content(active_tab, tab)
     active_tab == tab ? "<p>#{tab}</p>" : "<a href='/#{tab}'>#{tab}</a>"
   end
+  
+  include Sinatra::OutputBuffer::Helpers
 end
 
 before do
@@ -13,9 +15,6 @@ before do
   @data_sources_dir = DATA_SOURCES_DIR
   @segments_dir     = SEGMENTS_DIR
 end
-
-
-
 
 
 get %r{/$|/home$} do
@@ -27,7 +26,7 @@ end
 
 get '/segments' do
   @active_tab = :segments
-  @segments = Dir.directories(@segments_dir).sort
+  @segments = Dir.directories(@segments_dir).sort.collect{ |dir| Scarcity::Segment.new(File.join(@segments_dir, dir))}
   erb :segments
 end
 
@@ -35,6 +34,13 @@ get '/segments/:segment' do
   @active_tab = :segments
   @segment = Scarcity::Segment.new("#{@segments_dir}/#{params[:segment]}")
   erb :segment
+end
+
+get '/segments/:segment/:dataset' do
+  @active_tab = :segements
+  @segment = Scarcity::Segment.new("#{@segments_dir}/#{params[:segment]}")
+  @dataset = @segment.datasets.select {|d| d.data_id == params[:dataset]}.first
+  erb :dataset
 end
 
 get '/data' do
